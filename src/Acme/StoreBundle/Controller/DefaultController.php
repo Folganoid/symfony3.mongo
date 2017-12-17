767bd6f7ec837b34e513cb7e39b2733a6921d290
Andrey <folganoid@yahoo.com>
2017-12-17 19:47:33 +0200

    first commit

---
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Acme\StoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Acme\StoreBundle\Document\Product;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AcmeStoreBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/m")
+     */
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
+
+        return new Response('Created product id '.$product->getId());
+    }
+
+    /**
+     * @Route("/r/{id}")
+     */
+    public function showAction($id)
+    {
+        $product = $this->get('doctrine_mongodb')
+            ->getRepository('AcmeStoreBundle:Product')
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+
+        return new Response(var_dump($product));
+    }
+
+}
